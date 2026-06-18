4e617bc31731357470ad2a72b7eb47eb450c99b5
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-06-18 09:11:17 +0530

    add TwoDArray1 class for matrix search functionality

--- TwoDArray.java
+++ TwoDArray.java
@@ -25,3 +25,35 @@ public class TwoDArray {
 
     }
 }
+
+
+
+class TwoDArray1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        int matrix[][] = new int[row][col];
+
+        //input
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        //output
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j] == x){
+                    System.out.println(i+", "+j);
+                }
+            }
+        }
+
+    }
+}
+
