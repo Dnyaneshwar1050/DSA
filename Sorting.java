6d9b4b0cfba266741f7fd84014868a299b282c24
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-06-19 17:59:35 +0530

    created insertion sort code

--- Sorting.java
+++ Sorting.java
@@ -49,3 +49,29 @@ class Sorting1 {
         printSortedArr(arr);
     }
 }
+
+
+class Sorting2 {
+
+    public static void printSortedArr(int arr[]){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {7, 8, 3, 1, 2};
+
+        //insertion sort
+        for(int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            //placement 
+            arr[j+1] = current;
+        }
+        printSortedArr(arr);
+    }
+}
