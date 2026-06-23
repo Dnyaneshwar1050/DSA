b8fbecf577f8e39e6d528aef9710d4e885236e98
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-06-23 15:31:14 +0530

    SOLVE FIBONACCI SERIES QUESTION

--- Recursion1.java
+++ Recursion1.java
@@ -42,3 +42,32 @@ class Recursion2 {
     }
 
 }
+
+
+// print fibonacci series 
+
+class Recursion3 {
+
+    public static void printFibonacci(int a, int b, int n){
+
+        if(n == 0){
+            return;
+        }
+        int c = a+b;
+        System.out.println(c);
+        printFibonacci(b, c, n-1);
+    }
+
+    public static void main(String args[]){
+
+        int a = 0, b = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        printFibonacci(a, b, n-2);
+
+
+    }
+
+}
