09b10624d6ac8939944a458141fae30aafdd82c5
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-07-17 01:00:15 +0530

    add reverse stack functionality with StackClass1

--- StackClass.java
+++ StackClass.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+//push at buttom
+
 public class StackClass {
 
     public static void pushAtButtom(int data, Stack<Integer> s){
@@ -19,6 +21,48 @@ public class StackClass {
         s.push(3);
 
         pushAtButtom(4, s);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
+
+
+
+// reverse stack 
+
+
+class StackClass1 {
+
+    public static void pushAtButtom(int data, Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtButtom(data, s);
+        s.push(top);
+    }
+
+    public static void revese(Stack<Integer>s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        revese(s);
+        pushAtButtom(top, s);
+
+    }
+    public static void main(String arg[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        revese(s);
 
         while(!s.isEmpty()){
             System.out.println(s.peek());
