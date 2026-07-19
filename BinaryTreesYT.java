06adcfd5f0b468e33028212839f2fa53d5eab700
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-07-19 22:06:43 +0530

    add isIdentical and isSubtree methods for tree structure comparison

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -1,3 +1,5 @@
+import javax.swing.tree.TreeNode;
+
 public class BinaryTreesYT {
    static class Node {
        int data;
@@ -73,6 +75,33 @@ public class BinaryTreesYT {
 
        return Math.max(diam1, Math.max(diam2, diam3));
    }
+
+   public boolean isIdentical(TreeNode root,TreeNode subRoot){
+       if(subRoot == null && root == null){
+           return true;
+       }
+       if(root == null || subRoot == null){
+           return false;
+       }
+       if(root.val == subRoot.val){
+           return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+       }
+       return false;
+   }
+  
+   public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+       if(subRoot == null){
+           return true;
+       }
+       if(root == null){
+           return false;
+       }
+       if(isIdentical(root, subRoot)){
+           return true;
+       }
+       return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+   }
+
 
 
 
